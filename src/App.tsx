f71de16f674ade39f06c9ee14f53471e1ca0b180
Koro0 <ledane92@gmail.com>
2023-02-24 15:13:17 +0100

    add path

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header.component';
 import Index from './pages/Home/home';
+import Context from './components/Context/Context.component';
 
 import './styles/App.css';
 
 function App() {
+  const [darkMode, setDarkMode ]= useState(false)
+  
   return (
     <BrowserRouter>
+    <Context.Provider value={{darkMode, setDarkMode }}>
     <Header />
     <Routes>
       <Route path='/' element={<Index />} />
+      <Route path='/Login' element={<Index />} />
+      <Route path='/Register' element={<Index />} />
+      <Route path='/' element={<Index />} />
     </Routes>
+    </Context.Provider>
     </BrowserRouter>
     );
 }
