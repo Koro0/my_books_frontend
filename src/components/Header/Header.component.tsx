4c707d9e11576550381a2fb59c215e63c82bb6a1
Koro0 <ledane92@gmail.com>
2023-02-24 15:13:44 +0100

    add contexte theme in header

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -1,22 +1,44 @@
-import React from 'react'
+import { useContext } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Context from '../Context/Context.component';
+
+import Logo from '../Logo';
 
 export default function Header() {
+  const  {darkMode, setDarkMode}= useContext(Context);
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">Navbar</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <a className="nav-item nav-link active" href="/">Home <span className="sr-only">(current)</span></a>
-          <a className="nav-item nav-link" href="/">Features</a>
-          <a className="nav-item nav-link" href="/">Pricing</a>
-          <a className="nav-item nav-link disabled" href="/">Disabled</a>
-        </div>
-      </div>
-    </nav>
-    </div>
-  )
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+          <Logo />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Novel" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Recent</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                <span className='hotText'>Hot Novels</span>
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <div onClick={setDarkMode(!darkMode)} id="ligth">
+              Dank memes
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
