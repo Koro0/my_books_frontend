dfbd33aceb3a21afba4317e2d14136a904247516
Koro0 <ledane92@gmail.com>
2023-03-06 21:36:13 +0100

    add recipe path

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import {useState} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header.component';
-import Recipes from './pages/Recipe/recipes.page';
+import Recipes from './pages/Recipes/recipes.page';
 import Context from './components/Context/Context.component';
 import UserInterface from './pages/Profile/User.page';
-import Connexions from './pages/Connexion/LoginRegister.page';
+import Connexions from './pages/Connexion/Connexion.page';
+import Recipe from './pages/Recipe/recipe.page';
 
 
 import './styles/App.css';
@@ -18,6 +19,7 @@ function App() {
     <Header />
     <Routes>
       <Route path='/home' element={<Recipes />} />
+      <Route path='/recipe/:id' element={<Recipe />} />
       <Route path='/Connexion' element={<Connexions />} />
       <Route path='/user' element={<UserInterface />} />
     </Routes>
