3c170309b62b035ce9f23758e9a13c0f04199c06
Koro0 <ledane92@gmail.com>
2023-03-08 20:57:38 +0100

    create footer

---
+++ src/components/Footer/Footer.component.tsx
@@ -0,0 +1,26 @@
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <div className="footer__Box">
+                <div className="footer__Social">
+                    <span>social icons here</span>
+                    <span>social icons here</span>
+                    <span>social icons here</span>
+                </div>
+                <div>
+                    <h3>Recette</h3>
+                    <ul>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>..</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Thematique</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Footer
