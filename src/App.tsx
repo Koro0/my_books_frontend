f6656a6ded26c964aced5af52ed6a37c591685c0
Koro0 <ledane92@gmail.com>
2023-03-08 19:54:04 +0100

    upgrade semantic

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import Context from './components/Context/Context.component';
 import UserInterface from './pages/Profile/User.page';
 import Connexions from './pages/Connexion/Connexion.page';
 import Recipe from './pages/Recipe/recipe.page';
-
+import Index from './pages/Index/Index.page';
+import Footer from './components/Footer/Footer.component';
 
 import './styles/App.css';
 
@@ -15,15 +16,23 @@ function App() {
   
   return (
     <BrowserRouter>
-    <Context.Provider value={{darkMode, setDarkMode }}>
-    <Header />
-    <Routes>
-      <Route path='/home' element={<Recipes />} />
-      <Route path='/recipe/:id' element={<Recipe />} />
-      <Route path='/Connexion' element={<Connexions />} />
-      <Route path='/user' element={<UserInterface />} />
-    </Routes>
-    </Context.Provider>
+      <Context.Provider value={{darkMode, setDarkMode }}>
+        <header>
+            <Header />
+        </header>
+        <main>
+          <Routes>
+            <Route path='/' element={<Index />} />
+            <Route path='/home' element={<Recipes />} />
+            <Route path='/recipe/:id' element={<Recipe />} />
+            <Route path='/Connexion' element={<Connexions />} />
+            <Route path='/user' element={<UserInterface />} />
+          </Routes>
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </Context.Provider>
     </BrowserRouter>
     );
 }
