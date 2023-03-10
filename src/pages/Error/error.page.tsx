dbded461a3b00728f3c07ab1957b695a78114e6a
Koro0 <ledane92@gmail.com>
2023-03-10 14:49:51 +0100

    create error page

---
+++ src/pages/Error/error.page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Error() {
+  return (
+    <div>
+        <h1>404</h1>
+        <h2>OUPSS !</h2>
+        <h2>Page inexistante !!</h2>
+    </div>
+  )
+}
