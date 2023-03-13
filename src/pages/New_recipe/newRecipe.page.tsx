56a399e2ef1f9609a1aff993cd71638836d80011
Koro0 <ledane92@gmail.com>
2023-03-13 16:28:14 +0100

    add new recipe page

---
+++ src/pages/New_recipe/newRecipe.page.tsx
@@ -0,0 +1,10 @@
+import NewRecipeForm from "../../components/utils/newRecipeForm.component";
+
+export default function newRecipe() {
+  return (
+    <div>
+        <h1>Ajouter votre recette</h1>
+        <NewRecipeForm />
+    </div>
+  )
+}
