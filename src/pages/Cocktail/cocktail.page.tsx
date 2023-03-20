cc134f4b2dcf675da9a4880a954996d3ede507db
Koro0 <ledane92@gmail.com>
2023-03-20 14:49:05 +0100

    modify cocktailpage

--- src/pages/Cocktail/cocktail.page.tsx
+++ src/pages/Cocktail/cocktail.page.tsx
@@ -1,53 +1,30 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import { useNavigate } from 'react-router-dom';
+import React, {  useEffect, useState } from 'react';
+import CocktailList from '../../components/Cocktail/cocktailList.component';
+import CocktailOne from '../../components/Cocktail/cocktailOne.component';
 
-interface Drinks {
-    id:string;
-    title:string;
-    image:string;
-    ingredients:string;
-    difficulty:string;
-}
-export default function Cocktail() {
-    const [cocktailData, setCocktailData]=useState<Drinks[]>();
-    const navigate = useNavigate();
-    const config = {
-        method: 'GET',
-        headers: {
-          'X-RapidAPI-Key': '27b55ce60fmsh7b60b336e57e5fdp1adba0jsnb4b2bdb12282',
-          'X-RapidAPI-Host': 'the-cocktail-db3.p.rapidapi.com'
-        }
-      };
+export default function cocktails() {
+    const [getOneCocktail, setGetOneCocktail] = useState!<boolean>(false);
 
-    useEffect(()=> {
-        const fetchCocktail = async () => {
-            await axios.get('https://the-cocktail-db3.p.rapidapi.com/', config
-             ).then(({data}:{data:Drinks[]}) => {
-                setCocktailData(data);
-            }).catch(() => {
-                console.error();
-            });
+    useEffect!(()=>{
+        const url =new URL(window.location.href);
+        const cocktail_id = url.searchParams.get("id");
+        const verifLocation = () => {
+            if(cocktail_id === null ) {
+                setGetOneCocktail(false);
+            } else {
+                setGetOneCocktail(true);
+            }
         }
-        fetchCocktail()
+        console.log(getOneCocktail, cocktail_id )
+        verifLocation()
     }, [])
-   
   return (
-    <div  className='cocktail'>
-        {cocktailData && cocktailData.map((data)=> {
-            return (
-            <Card style={{ width: '18rem' }} className='cocktail__card' key={data.id}>
-                <Card.Img  className='cocktail__img' src={data.image} alt={data.title} />
-                <Card.Body>
-                <Card.Title className='cocktail__title'>{data.title}</Card.Title>
-                <Card.Text> Difficulty : {data.difficulty}</Card.Text>
-                </Card.Body>
-                <Button onClick={()=> navigate(`/${data.id}`)} variant="primary">Go recipe !</Button>
-            </Card>
-            )
-        })}
+    <div>
+        <h1>récent cocktail</h1>
+        <div>
+            {!getOneCocktail ? <CocktailList /> : <CocktailOne />}
+        
+        </div>
     </div>
   )
 }
