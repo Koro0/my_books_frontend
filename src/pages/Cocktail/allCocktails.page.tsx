a9a753a3bad633bc3e7d7950423e98d397ecf5fb
Koro0 <ledane92@gmail.com>
2023-03-31 16:07:40 +0200

    separate cocktails pages

---
+++ src/pages/Cocktail/allCocktails.page.tsx
@@ -0,0 +1,13 @@
+import CocktailList from '../../components/Cocktail/cocktailList.component';
+
+export default function AllCocktails() {
+
+  return (
+    <div>
+        <h1>récent cocktail</h1>
+        <div>
+            <CocktailList /> 
+        </div>
+    </div>
+  )
+}
--- src/pages/Cocktail/cocktail.page.tsx
+++ src/pages/Cocktail/cocktail.page.tsx
@@ -1,29 +1,11 @@
-import React, {  useEffect, useState } from 'react';
-import CocktailList from '../../components/Cocktail/cocktailList.component';
 import CocktailOne from '../../components/Cocktail/cocktailOne.component';
 
-export default function cocktails() {
-    const [getOneCocktail, setGetOneCocktail] = useState!<boolean>(false);
+export default function Cocktails() {
 
-    useEffect!(()=>{
-        const url =new URL(window.location.href);
-        const cocktail_id = url.searchParams.get("id");
-        const verifLocation = () => {
-            if(cocktail_id === null ) {
-                setGetOneCocktail(false);
-            } else {
-                setGetOneCocktail(true);
-            }
-        }
-        console.log(getOneCocktail, cocktail_id )
-        verifLocation()
-    }, [])
   return (
     <div>
-        <h1>récent cocktail</h1>
         <div>
-            {!getOneCocktail ? <CocktailList /> : <CocktailOne />}
-        
+            <CocktailOne />
         </div>
     </div>
   )
