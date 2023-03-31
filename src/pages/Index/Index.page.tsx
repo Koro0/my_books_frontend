8b73433db9127f0af02206a978618a6313d5ef44
Koro0 <ledane92@gmail.com>
2023-03-31 16:07:52 +0200

    add class

--- src/pages/Index/Index.page.tsx
+++ src/pages/Index/Index.page.tsx
@@ -4,14 +4,14 @@ import Context from '../../components/Context/Context.component';
 export default function Index() {
     const  {darkMode}= useContext(Context);
   return (
-    <div className={!darkMode?"darkMode":"lightMode"}>
+    <div className={(!darkMode?"darkMode":"lightMode") + ' index'}>
         <h1>Bienvenu sur mon site</h1>
         <p>Mon site est dédié au recette de cuisine et la lecture de roman.</p>
         <p>De nouvelle outil sera ajouter au fils du temps et des propositions.</p>
         <div>
         <h2>Organisation : </h2>
         <p>Nous aurons des recettes classaient dans different catégorie dans le future</p>
-        <p>Ca sera de la même manièrepour les romans</p>
+        <p>Ca sera de la même manière pour les romans</p>
         </div>
     </div>
   )
