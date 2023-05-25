ce21dcd6beb792ae362eeb977b21232a8815a628
Koro0 <ledane92@gmail.com>
2023-05-25 19:15:48 +0200

    het all comments

--- src/components/Comments/Comments.component.tsx
+++ src/components/Comments/Comments.component.tsx
@@ -2,30 +2,42 @@ import axios from 'axios'
 import {useEffect, useState} from 'react'
 
   interface Comments {
-
+    commentId:number,
+    commentText:string,
+    createdAt:Date,
+    userId:number,
   }
- const ShowComments = (props: {cocktailId : number}) => {
-    const [commentData, setCommentData] = useState<Comments>([]);
+  interface ShowCommentsProps {
+    categories:string,
+    ID : number
+  }
+ const ShowComments = (props: ShowCommentsProps) => {
+    const [commentData, setCommentData] = useState<Comments[]>();
 
     useEffect(() => {
       const fetchCocktailComments =async () => {
-       await axios.get('http://localhost:3500/api/comment/'+props.cocktailId, {
+       await axios.get('http://localhost:3500/api/comment/'+props.categories+"/"+props.ID, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('connect')}`,
         }
       })
-      .then((data)=> {setCommentData(data)})
+      .then((data)=> {setCommentData(data.data)})
       .catch((err)=> {console.error(err)})};
       fetchCocktailComments();
-    }, [])
+    }, [props])
     
   return (
-    <>
-        {/*commentData && commentData.map((data) => {
+    <div>
+        {commentData && commentData.map((datas) => {
+          return (
+            <article key={datas.commentId}>
+              <h2>{datas.userId}</h2>
+              <p>{datas.commentText}</p>
+            </article>
+          )
+        })}
 
-        })*/}
-
-    </>
+    </div>
   )
 }
 export default ShowComments
