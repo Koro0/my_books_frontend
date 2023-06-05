cad1d51a83f8bd1d6a25621ad40f3c528f42369f
Koro0 <ledane92@gmail.com>
2023-06-05 21:04:49 +0200

    show one recipe

--- src/pages/Recipe/recipe.page.tsx
+++ src/pages/Recipe/recipe.page.tsx
@@ -1,46 +1,75 @@
 import React, {useEffect, useState} from 'react';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
-import { Recipes } from '../../components/Interface';
+import { Recipes, RecipeRouteParams, IngredientItem, MethodStep } from '../../components/Interface';
 
-export default function recipe() {
-  const url = window.location.href;
-  const Id = url.split('/').pop();
-  const [recipeData, setRecipeData]=useState!<Recipes[]>()
+export default function ShowOneRecipe() {
+  const {recipeID} = useParams<RecipeRouteParams>();
+  const [recipeData, setRecipeData]=useState<Recipes>();
+  const [ingredientData, setIngredientData]=useState<IngredientItem[]>([]);
+  const [methodData, setMethodData]=useState<MethodStep[]>();
+
   useEffect(() => {
     const fetchRecipe = async () => {
-      await axios.get("http://localhost:3500/api/recipe/"+ Id, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('connect')}`,
-        }
-      }).then(({data}) => {
-        setRecipeData(data.data);
-    }).catch(() => {
+      await axios.get("http://localhost:3500/api/recipe/"+ recipeID, 
+      ).then(({data}) => {
+        setRecipeData(data.recipe);
+        setIngredientData(data.ingredients);
+        setMethodData(data.methods);
+      }).catch(() => {
         console.error();
-    });
+      });
     }
     fetchRecipe();
-  }, [Id])
-  
+  }, [recipeID, recipeData])
+  console.log(recipeID);
 
   return (
     <>
-      {recipeData && recipeData.map((datas)=> {
-        return(
-          <section key={datas.recipeId}>
-            <h2>{datas.title}</h2>
-            <img src={datas.image} alt="" />
+      {recipeData && 
+          <section>
+            <h2>{recipeData.title}</h2>
+            <img src={recipeData.image} alt="" />
             <article>
               <p>
-                {datas.description}
+                {recipeData.description}
               </p>
-              </article>
+            </article>
             <article>
-              <p>{datas.portion}</p>
+              <p>{recipeData.portion}</p>
+            </article>
+            <article>
+              <h3>Liste d'ingredient : </h3>
+              <ul>
+              {ingredientData.map((item)=> {
+              if(item.cocktailId === recipeData.recipeId) {
+                return (
+                    <li key={item.ingredientId}>
+                      <p>  {item.quantity},  {item.name} </p>
+                    </li>
+                )
+              }
+              return null;
+            })}
+              </ul>
+            </article>
+            <article>
+              <h3>Les Etapes : </h3>
+              {methodData?.map((step) => {
+                if(step.cocktailId === recipeData.recipeId) {
+                  return (
+                      <li key={step.recipeId}>
+                        <h6> Method {step.stepNumber} : </h6>
+                        <p> {step.description} </p>
+                      </li>
+                  )
+                }
+                return null;
+              })}
             </article>
           </section>
-        )
-      })}
+      }
     </>
   )
 }
