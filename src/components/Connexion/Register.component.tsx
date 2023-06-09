1b32d5a991c46cefa56bc17b0a9d01e9144eadca
Koro0 <ledane92@gmail.com>
2023-06-09 14:06:25 +0200

    remove error data

--- src/components/Connexion/Register.component.tsx
+++ src/components/Connexion/Register.component.tsx
@@ -1,39 +1,46 @@
+import axios from "axios";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 interface FormInput {
-  Email:string,
-  Password:string,
-  Username:string
+  email:string,
+  password:string,
+  pseudo:string
 }
 export default function Register() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors }
   } = useForm<FormInput>();
 
-  console.log(watch("Password", "Email",)); // you can watch individual input by pass the name of the input
-
-  const onSubmit:SubmitHandler<FormInput> = data => console.log(data);
+  const onSubmit:SubmitHandler<FormInput> =async (data) =>{
+    await axios.post("http://localhost:3500/api/users/register",data, {
+       headers: {
+          'Content-Type': 'application/json'
+          },
+      })
+      .then(()=> console.log('Account was created'))
+      .catch(()=> console.log('error register'));
+      console.log(JSON.stringify(data));
+  };
 
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
     >
       <label>Email</label>
-      <input {...register("Email", {required: true, pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g})} defaultValue="" />
-      {errors.Email && <p>Email is required</p>}
+      <input {...register("email", {required: true, pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g})} defaultValue="" />
+      {errors.email && <p className="errorMsg">Email is required</p>}
       <label>Password</label>
       <input
-        {...register("Password", { required: true, maxLength: 10 })}
+        {...register("password", { required: true,  minLength: 8, maxLength: 20 })}
       />
-      {errors.Password && <p>Password is required</p>}
+      {errors.password && <p className="errorMsg">Password is required</p>}
       <label>Pseudo :</label>
       <input
-        {...register("Username", { required: true, maxLength: 20 })}
+        {...register("pseudo", { required: true, minLength: 5, maxLength: 20 })}
       />
-      {errors.Password && <p>User Name is required</p>}
+      {errors.pseudo && <p className="errorMsg">User Name is required</p>}
       <input type="submit" />
     </form>
   );
