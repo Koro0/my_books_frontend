03c79277d382064a9a6a9961f4068bc235774312
Koro0 <ledane92@gmail.com>
2023-07-07 23:04:49 +0200

    clean code

--- src/components/Footer/Footer.component.tsx
+++ src/components/Footer/Footer.component.tsx
@@ -2,27 +2,9 @@
 const Footer = () => {
     const toDate:Date = new Date(Date.now());
     return(
-        <div className="footer">
-            {/*<div className="footer__Box">
-                <div className="footer__Social">
-                    <p>social icons </p>
-                    <p>social icons </p>
-                    <p>social icons </p>
-                </div>
-                <div className="footer__Recipe">
-                    <h3>Recette</h3>
-                    <ul>
-                        <li>1</li>
-                        <li>2</li>
-                        <li>..</li>
-                    </ul>
-                </div>
-                <div className="footer__Thematique">
-                    <h3>Thematique</h3>
-                </div>
-            </div>*/}
+        <footer className="footer">
             <div className="footer__Copy">Copyright YD  {toDate.getFullYear()} </div>
-        </div>
+        </footer>
     )
 }
 export default Footer
