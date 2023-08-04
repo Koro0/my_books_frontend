5d4e076e94c854e5f5c53780491daca299494948
Koro0 <ledane92@gmail.com>
2023-08-05 00:14:36 +0200

    change name

---
+++ src/pages/Novels/novels.page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ShowNovels from '../../components/Novels/Novels.component'
+
+export default function Novels() {
+  return (
+    <div>
+        <h1>Novels</h1>
+        <ShowNovels />
+    </div>
+  )
+}
