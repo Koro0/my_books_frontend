928ca72e9d45ee4cbeea8f5ccffc5eb8e1c21710
Koro0 <ledane92@gmail.com>
2023-10-05 13:17:04 +0200

    add inteface showCardsProps

--- src/components/Interface.ts
+++ src/components/Interface.ts
@@ -1,115 +1,124 @@
-
 export interface Drink {
-    cocktailId:number,
-    title:string,
-    description:string,
-    portion:string,
-    time:string,
-    difficulty:string,
+  cocktailId: number;
+  title: string;
+  description: string;
+  portion: string;
+  time: string;
+  difficulty: string;
+  image: string;
 }
 
 export interface Drinks {
-    cocktailId:number,
-    title:string,
-    description:string,
-    portion:string,
-    time:string,
-    difficulty:string,
+  cocktailId: number;
+  title: string;
+  description: string;
+  portion: string;
+  time: string;
+  difficulty: string;
+  image: string;
 }
 
 export interface IngredientItem {
-    ingredientId:number,
-    cocktailId:number,
-    recipeId:number,
-    name:string,
-    quantity:string,
+  ingredientId: number;
+  cocktailId: number;
+  recipeId: number;
+  name: string;
+  quantity: string;
 }
 
 export interface MethodStep {
-    methodId:number,
-    cocktailId:number,
-    recipeId:number,
-    stepNumber:number,
-    description:string,
+  methodId: number;
+  cocktailId: number;
+  recipeId: number;
+  stepNumber: number;
+  description: string;
 }
 
 export type CocktailRouteParams = {
-    cocktailId:string,
-}
+  cocktailId: string;
+};
 
 export type RecipeRouteParams = {
-    recipeID:string,
-}
+  recipeID: string;
+};
 
 export type NovelRouteParams = {
-    novelId:string,
-}
+  novelId: string;
+};
 
 export interface CocktailProps {
-    cocktail_id:string,
+  cocktail_id: string;
 }
 
 export interface Comment {
-    commentId:number,
-    commentText:string,
-    userId:number,
-    novelId: number,
-    recipeId: number,
-    cocktailId: number,
-    createdAt:Date,
+  commentId: number;
+  commentText: string;
+  userId: number;
+  novelId: number;
+  recipeId: number;
+  cocktailId: number;
+  createdAt: Date;
 }
 
 export interface ShowCommentsProps {
-categories:string,
-ID : number
+  categories: string;
+  ID: number;
 }
 
 export interface createComment {
-    commentText:string, 
-    userId:number
+  commentText: string;
+  userId: number;
 }
 
 export interface Recipes {
-    recipeId:number,
-    title:string,
-    description:string,
-    image:string,
-    portion:string,
-    time:string,
-    addBy:number,
-    createdAt:Date
+  recipeId: number;
+  title: string;
+  image: string;
+  portion: string;
+  time: string;
+  createdAt: Date;
 }
 
 export type Recipe = {
-    recipeId:number,
-    title:string,
-    image:string | null,
-    content:string | null,
-    author:number,
-    createdAt: Date,
-  }
+  recipeId: number;
+  title: string;
+  description: string;
+  image: string | null;
+  content: string | null;
+  author: number;
+  createdAt: Date;
+};
 
 export interface Novel {
-    novelId:number,
-    image:string,
-    title:string,
-    type:string,
-    author:string,
-    description:string,
-    likesCount:number,
-    createdAt:Date,
+  novelId: number;
+  image: string;
+  title: string;
+  type: string;
+  author: string;
+  description: string;
+  likesCount: number;
+  createdAt: Date;
 }
 
 export interface ChapterAttributes {
-    chapterId:number,
-    chapterNumber:number,
-    title:string,
-    novelId:number,
-    createdAt:Date
-  }
+  chapterId: number;
+  chapterNumber: number;
+  title: string;
+  novelId: number;
+  createdAt: Date;
+}
 
 export interface Paragraphs {
-    paragraphId:number,
-    paragraph:string, 
-    chapterId: number,
+  paragraphId: number;
+  paragraph: string;
+  chapterId: number;
+}
+
+export interface ShowCardsProps {
+  itemId: number;
+  itemTitle: string;
+  itemLink: string;
+  itemImg: string;
+  recipePortion?: number;
+  recipeTime?: string;
 }
