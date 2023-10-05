325a877e8610cfc07c860f0c1c97d0c925f8e16d
Koro0 <ledane92@gmail.com>
2023-10-05 16:34:48 +0200

    clean code

--- src/pages/Cocktail/allCocktails.page.tsx
+++ src/pages/Cocktail/allCocktails.page.tsx
@@ -1,13 +1,12 @@
 import CocktailList from '../../components/Cocktail/cocktailList.component';
 
 export default function AllCocktails() {
-
   return (
-    <div>
-        <h1>récent cocktail</h1>
-        <div>
-            <CocktailList /> 
-        </div>
-    </div>
-  )
+    <>
+      <h1>récent cocktail</h1>
+      <div>
+        <CocktailList />
+      </div>
+    </>
+  );
 }
