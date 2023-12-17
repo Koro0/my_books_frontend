9d98e46a626897f8cd8bd847c7da92b63e1860c8
Koro0 <ledane92@gmail.com>
2023-12-17 23:03:20 +0100

    add btn

--- src/pages/Connexion/Connexion.page.tsx
+++ src/pages/Connexion/Connexion.page.tsx
@@ -1,20 +1,30 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import Login from '../../components/Connexion/Login.component';
 import Register from '../../components/Connexion/Register.component';
 
- function Connexions() {
-    const [loginRegister, setLoginRegister] = useState<boolean>(false)
+function Connexions() {
+  const [loginRegister, setLoginRegister] = useState<boolean>(false);
   return (
-    <div id='connexion'>
-      <h1>{!loginRegister ? 'Login' : 'Register'}</h1>
-      <div>
-      {!loginRegister ? <Login /> : <Register />}
-      </div>
-      <div className="connexion_accountOrNot">
-        {!loginRegister ? (<p>Pas de compte ? </p>) : (<p>Vous avez un compte ? </p> ) }
-        <button className='connexion_switchBtn' onClick={()=> setLoginRegister(!loginRegister)}> {!loginRegister ? 'Inscrivez-vous' : 'Connectez-vous'}</button> 
+    <div className="connexion">
+      <div className="connexion_box">
+        <h1>{!loginRegister ? 'Login' : 'Register'}</h1>
+        <div>{!loginRegister ? <Login /> : <Register />}</div>
+        <div className="connexion_accountOrNot">
+          {!loginRegister ? (
+            <p>Pas de compte ? </p>
+          ) : (
+            <p>Vous avez un compte ? </p>
+          )}
+          <button
+            className="connexion_switchBtn"
+            onClick={() => setLoginRegister(!loginRegister)}
+          >
+            {' '}
+            {!loginRegister ? 'Inscrivez-vous' : 'Connectez-vous'}
+          </button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
-export default Connexions
+export default Connexions;
